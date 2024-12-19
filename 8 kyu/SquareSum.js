1b7ccce21265c7c3ea4ca970e65d18fd5cb36b2d
KennyKvn001 <k.mugisha@alustudent.com>
2024-12-19 10:17:41 +0200

    Square sum

--- 8 kyu/SquareSum.js
+++ 8 kyu/SquareSum.js
@@ -1,3 +1,8 @@
 function squareSum(numbers) {
   return numbers.map((num) => num ** 2).reduce((a, b) => a + b, 0);
 }
+
+// Best practice
+function squareSum(numbers) {
+  return numbers.reduce((sum, num) => sum + num * num, 0);
+}
