5dfef1c2866407799c7bc847d8c92ff252020531
KennyKvn001 <k.mugisha@alustudent.com>
2025-02-25 22:40:41 +0200

    Bouncing Ball

---
+++ 6 kyu/Question.js
@@ -0,0 +1,15 @@
+function bouncingBall(h,  bounce,  window) {
+    if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) {
+      return -1;
+    }
+    
+    let count = 1
+    
+    while (h * bounce > window) { 
+      h *= bounce;
+      count += 2; 
+    }
+  
+    return count;
+    
+  }
